0987f709d180dc478990524fd6f39e7fc92a9a82
Jan Bílek <honza@elision.design>
2020-02-23 12:53:51 +0100

    Create auth wrapper

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,26 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 
+// Import the Auth0 configuration
+import { domain, clientId } from '../auth_config.json';
+
+// Import the plugin here
+import { Auth0Plugin } from './auth';
+
+// Install the authentication plugin here
+Vue.use(Auth0Plugin, {
+  domain,
+  clientId,
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  onRedirectCallback: (appState: any) => {
+    router.push(
+      appState && appState.targetUrl
+        ? appState.targetUrl
+        : window.location.pathname,
+    );
+  },
+});
+
 Vue.config.productionTip = false;
 
 new Vue({
